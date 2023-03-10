7d68c9f02a94edd782db62a7e8fb629e10d74edc
ebi <ebi.mohamadi63@gmail.com>
2023-03-10 17:38:33 +0000

    in pathway

--- Tahlildade/part3/tamrin.py
+++ Tahlildade/part3/tamrin.py
@@ -11,3 +11,25 @@ for i in range(3):
             print("*")
 
 print("==============tamrin 2 =================")
+
+string = []
+
+while(True): 
+    word = input("Please enter your str: ")
+    if word.lower() == 'break':
+        break
+    else:
+        string.append(word)
+print(string)
+
+print("==============tamrin 3 =================")
+
+num_list = []
+for i in range(10):
+    num_list.append(input("Please enter a number: "))
+
+num_list.sort()
+
+print(num_list)
+
+print("==============tamrin 4 =================")
